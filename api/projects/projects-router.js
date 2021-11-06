e3436926452cf470deefb66e4bd702a1ce184d88
LuiCoro <luiacorona029@icloud.com>
2021-11-05 21:53:28 -0700

    '[GET] /api/projects ( pre-middleware )' - passes both tests successfully

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,32 @@
 // Write your "projects" router here!
+
+const express = require('express');
+
+const Project = require('./projects-model')
+
+const router = express.Router()
+
+// [GET] /api/projects
+router.get('/', async (req, res) => {
+  try {
+    const projects = await Project.get()
+    if(!projects) {
+      res.status(404).json([])
+    } else {
+      res.status(200).json(projects)
+    }
+  } catch (err) {
+    res.status(500).json({message: 'Error with the request'})
+  }
+})
+
+
+
+module.exports = router
+
+
+// GET USERS BY ID
+// POST PROJECTS
+// PUT PROJECTS BY ID
+// DELETE BY ID
+// READ ( README )!
